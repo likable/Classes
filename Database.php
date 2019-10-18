d548bc06256dcc379b3d5dd4cdad465ab961bd37
Виталий <wental-@mail.ru>
2019-10-18 10:30:45 +0300

    Database class created. Define structure and write magic methods.

---
+++ Database.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace classes;
+
+use PDO;
+
+class Database 
+{
+    private $pdo;
+
+    public static $charset = "utf8";
+    public static $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false
+    ];
+    
+    public function __construct($host, $db_name, $user_name, $password) 
+    {
+        $dsn = "mysql:host={$host}; dbname={$db_name}; charset=" . self::$charset;
+        $this->pdo = new PDO($dsn, $user_name, $password, self::$options);
+    }
+    
+    public function __destruct()
+    {
+        $this->pdo = null;
+    }
+    
+    //query
+    private function getSTMT($query, $params = [])
+    {
+        $stmt = false;
+        try {
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute($params);
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+        return $stmt;
+    }
+    
+    
+    
+    //getRow
+    public function getRow($query, $params = [])
+    {
+        return $this->getSTMT($query, $params)->fetch();
+    }
+    
+    
+    
+    //getRows
+    
+    
+    
+    //getColumn
+    
+    
+    
+    //insert
+    
+    
+    
+    //update
+    
+    
+    
+    //delete
+    
+    
+    
+    //lastInsertId
+    
+    
+    
+    //numRows
+    
+    
+}
