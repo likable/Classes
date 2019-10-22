58475e244dd816593ce89339216015ba98e23fa0
Виталий <wental-@mail.ru>
2019-10-22 22:50:45 +0300

    parser class structure

---
+++ Parser.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace classes;
+
+class Parser 
+{
+    
+    //private var resource
+    
+    
+    
+    
+    //__constructor
+    
+    
+    
+    
+    //setopt
+    
+    
+    
+    
+    //getBody 
+    
+    
+    
+    
+    //getHeaders
+    
+    
+    
+    
+    //getStatusCode
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
