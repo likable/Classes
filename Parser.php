5d8c66746137a8d564a921974f08f23507cb9f1a
Виталий <wental-@mail.ru>
2019-10-23 12:02:14 +0300

    add __construct, __destruct, setopt, getBody methods

--- Parser.php
+++ Parser.php
@@ -5,22 +5,35 @@ namespace classes;
 class Parser 
 {
     
-    //private var resource
+    private $ch;
     
+    public function __construct($timeout = 60) 
+    {
+        $this->ch = curl_init();
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($this->ch, CURLOPT_TIMEOUT, $timeout);
+    }
     
+    public function __destruct() 
+    {
+        curl_close($this->ch);
+    }
     
+    public function setopt($name, $value)
+    {
+        if (!curl_setopt($this->ch, $name, $value)) {
+            die("Setopt {$name} failed!");
+        }
+    }
     
-    //__constructor
+    public function getBody($url)
+    {
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+        return curl_exec($this->ch);
+    }
     
-    
-    
-    
-    //setopt
-    
-    
-    
-    
-    //getBody 
     
     
     
